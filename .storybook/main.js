71e3bfc8ed6ecb470ecb284bba81f706692ba904
jiyoon0605 <ljy014@naver.com>
2021-06-04 10:12:25 +0900

    simple conponent

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ["../src/component/**/*.stories.js"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+  ],
+};
